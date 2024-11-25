d65151c1c4743dcc171a90a410bef64839ab3286
Yazan Aljaloudi <yhjaloudi@gmail.com>
2024-11-25 16:41:52 +0100

    ButtonComponent

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,9 +1,15 @@
 
 import ButtonGradient from './assets/svg/ButtonGradient';
+import Button from './componenets/Button';
 const App = () => {
   return (
     <>
     <h1 className="text-3xl font-bold underline">Hello</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" >
+        Seomthing
+        </Button>
+      </div>
       <ButtonGradient />
     </>
   )
